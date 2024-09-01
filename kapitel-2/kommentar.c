6330e51db6da395cc0f258a5363483c315d38a6e
reztib <bitzer2@gmx.de>
2024-09-02 00:45:32 +0200

    kommentare

---
+++ kapitel-2/kommentar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    /* Es gibt lange Kommentare.
+        Diese stehen in mehreren Zeilen */
+    printf("Es gibt auch lange"
+    " Texte.\nDiese stehen"
+    " auch in mehreren Zeilen.\n");
+
+    return 0; // Kommentar bis zum Ende der Zeile, seit C99
+}
